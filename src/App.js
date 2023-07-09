46cdb40d8a399731bc2467209db03386dd87ae55
Santosh <atharw010@gmail.com>
2023-07-09 12:46:40 +0530

    update ci= false

--- src/App.js
+++ src/App.js
@@ -1,55 +1,61 @@
-import React, {useState, useEffect} from 'react';
-import Recipe from './Recipe';
+import React,{useState,useEffect} from 'react';
 import './App.css';
 import axios from 'axios';
-
+import Recipe from './Recipe'
 
 function App() {
-
-  const APP_ID = "74de586e" ;
-  const APP_KEY = "29af486d82d10bce32f00d2ab4076d94" ;
-
+  const APP_ID = "c429c2cb";
+  const APP_key = "89c4802646e5c79f2760ffa2d556f0f4";
+  
   const [recipes, setRecipes] = useState([])
-  const [input, setInput] = useState("naan");
 
+  const [searchbar, setSearchBar] = useState('chicken')
 
   useEffect(() => {
-    getRecipes()
-  
+    newfun()
   }, [])
   
 
-
-  const getRecipes =  () =>{
-    axios.get(`https://api.edamam.com/api/recipes/v2?type=public&q=${input}&app_id=${APP_ID}&app_key=${APP_KEY}`)
-   .then(function (response) {
-    // handle success
-    console.log(response);
-    setRecipes(response.data.hits)
-    setInput("")
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-  })
+  function newfun() {
+    axios.get(`https://api.edamam.com/api/recipes/v2?type=public&q=${searchbar.toLocaleLowerCase()}&app_id=${APP_ID}&app_key=${APP_key}`)
+    .then(function (response) {
+      console.log(response);
+      // handle success
+      setRecipes(response.data.hits)
+      setSearchBar("")
+    })
+    .catch(function (error) {
+      // handle error
+    
+    })
   }
-  const onSubmitBtn =(e) =>{
 
-    e.preventDefault()
-    getRecipes();
+  function newfun2(e) {
+    e.preventDefault();
+    newfun();
   }
-console.log(recipes);
 
   return (
     <div className="App">
-      <form onSubmit={(e) => onSubmitBtn(e)} className='search-from'>
-      <input value={input} onChange={(e) => setInput(e.target.value)} className='search-bar' type='text' />
-      <button className='search-button' type='submit'>Search</button>
+      <form className='search-form' onSubmit={newfun2}>
+        <input value={searchbar} onChange = {(event) => setSearchBar(event.target.value)}  className='search-bar' type="text" placeholder='Enter Your Ingredients' />
+        <button  className='search-btn'>
+          Search
+        </button>
+        <div className='ddww'>
+        {recipes.map((recipe, index)=> {
+        return(
+          <Recipe key={index}
+          title = {recipe.recipe.label}
+          calories = {recipe.recipe.calories}
+          image = {recipe.recipe.image}
+          ingredients={recipe.recipe.ingredientLines}/>
+        )
+      })}
+      </div>
       </form>
-      {recipes.map(recipe => (
-        <Recipe  key={recipe.recipe.label} title={recipe.recipe.label} calories={recipe.recipe.calories} image={recipe.recipe.image}/>
-      ))}
-   </div>
+
+    </div>
   );
 }
 
--- src/Recipe.js
+++ src/Recipe.js
@@ -1,12 +1,17 @@
 import React from 'react'
+import "./Recipe.css"
 
-function Recipe({title,calories,image}) {
+function Recipe(props) {
   return (
-    <div className='recipe-card'>
-         <img src={image} alt='' />
-        <h1 className='text'>{title}</h1>
-        <p>{calories}</p>
-       
+    <div className='rep'>
+        <h1>{props.title}</h1>
+        <ol>
+            {props.ingredients.length > 0 && props.ingredients.map((data,index) => (
+                <li key={index}>{data}</li>
+            ))}
+        </ol>
+        <p className='rep-p'>calories: {props.calories}</p>
+        <img className='image' src={props.image} alt="" />
     </div>
   )
 }
